2bdd9af7f4a9f8c1ae9ae8aeda29a9acab965a2e
kavindu-madu <142071735+kavindu-madu@users.noreply.github.com>
2025-01-19 21:47:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
 > Ai Bot Password Get From Watching This Video 
 */
-var SESSION_ID = ''
+var SESSION_ID = 'QUEENELISA;;;==waO9VYJJTa2kmZtVzN4NXeVFmTtcWRjNGcBRnND52Xxx0NBNWVaJEarFXTjElaiRVerJlTpwd=93811427749'
 global.AI_PASSWORD = 'king-nima-gpt-bot'
 global.SESSION_ID = process.env.SESSION_ID || SESSION_ID 
 
